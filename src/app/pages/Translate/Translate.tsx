74af366df58b7ed480d6ec80edde2d5202405039
Aidar <abdykayimovaidar@gmail.com>
2023-08-12 23:39:24 +0300

    little fix

--- src/app/pages/Translate/Translate.tsx
+++ src/app/pages/Translate/Translate.tsx
@@ -1,112 +1,58 @@
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import axios from 'axios';
+import React, { useRef } from 'react';
 
 export const Translate = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-    const recordedChunksRef = useRef<Blob[]>([]);
-    const [isCameraOn, setIsCameraOn] = useState(false);
-    const [isRecording, setIsRecording] = useState(false);
-    const [addGptQuery, setAddGptQuery] = useState(false)
+    const chunksRef = useRef<Blob[]>([]);
 
-    const handleCheckboxChange = () => {
-        setAddGptQuery(prevState => !prevState);
-    };
+    const startRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+        }
 
-    useEffect(() => {
-        if (isCameraOn) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(stream => {
-                    if (videoRef.current) {
-                        videoRef.current.srcObject = stream;
-
-                        const mediaRecorder = new MediaRecorder(stream);
-                        mediaRecorder.ondataavailable = event => {
-                            if (event.data.size > 0) {
-                                recordedChunksRef.current.push(event.data);
-                            }
-                        };
-                        mediaRecorderRef.current = mediaRecorder;
-                    }
-                })
-                .catch(error => {
-                    console.error("Error accessing camera:", error);
-                });
-        } else {
-            const videoStream = videoRef.current?.srcObject as MediaStream | null;
-            if (videoStream) {
-                videoStream.getTracks().forEach(track => {
-                    track.stop();
-                });
-                if (videoRef.current) {
-                    videoRef.current.srcObject = null;
-                }
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                chunksRef.current.push(event.data);
             }
-        }
-    }, [isCameraOn]);
-
-    const handleToggleCamera = () => {
-        setIsCameraOn(prevIsCameraOn => !prevIsCameraOn);
-    };
-
-    const handleStartRecording = () => {
-        recordedChunksRef.current = [];
-        if (mediaRecorderRef.current) {
-            mediaRecorderRef.current.start();
-            setIsRecording(true);
-        }
-    };
-
-    const handleStopRecording = () => {
-        if (mediaRecorderRef.current && isRecording) {
-            mediaRecorderRef.current.stop();
-            setIsRecording(false);
-
-            const recordedBlob = new Blob(recordedChunksRef.current, { type: "video/mp4" });
-            sendVideoToAPI(recordedBlob);
-        }
-    };
-
-    const sendVideoToAPI = async (videoBlob: Blob) => {
-        try {
+        };
+        mediaRecorder.onstop = async () => {
+            const blob = new Blob(chunksRef.current, { type: 'video/webm' });
             const formData = new FormData();
-            formData.append("video", videoBlob);
-
-            const response = await axios.post("http://larek.itatmisis.ru:9000/sl2text", formData, {
-                params: {
-                    gpt: addGptQuery ? true : false
-                }
-            });
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-        }
+            formData.append('video', blob, 'recorded.webm');
+            try {
+                const response = await axios.post('http://larek.itatmisis.ru:9000/sl2text', formData);
+                console.log('Video uploaded successfully!', response.data);
+            } catch (error) {
+                console.error('Error uploading video:', error);
+            }
+            chunksRef.current = [];
+        };
+        mediaRecorderRef.current = mediaRecorder;
+        mediaRecorder.start();
     };
 
+    const stopRecording = () => {
+        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
+            mediaRecorderRef.current.stop();
+        }
+    };
     return (
-        <div className="w-full flex flex-col gap-3">
-            {/* Add a video element to display the camera feed */}
-            <video ref={videoRef} autoPlay style={{ width: "100%" }} />
-
-            {/* Buttons to toggle the camera and start/stop recording */}
-            <button onClick={handleToggleCamera} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                {isCameraOn ? "Turn Off Camera" : "Turn On Camera"}
-            </button>
-            <input
-                type="checkbox"
-                checked={addGptQuery}
-                onChange={handleCheckboxChange}
-            />
-            {isCameraOn && !isRecording && (
-                <button onClick={handleStartRecording} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl mb-4">Webcam Recorder</h1>
+            <div className="mb-4">
+                <video ref={videoRef} autoPlay playsInline className="w-full max-w-md" />
+            </div>
+            <div className="flex space-x-4">
+                <button onClick={startRecording} className="bg-blue-500 text-white px-4 py-2 rounded">
                     Start Recording
                 </button>
-            )}
-            {isRecording && (
-                <button onClick={handleStopRecording} className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                <button onClick={stopRecording} className="bg-red-500 text-white px-4 py-2 rounded">
                     Stop Recording
                 </button>
-            )}
+            </div>
         </div>
     );
 };
